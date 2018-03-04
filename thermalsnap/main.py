5b570b72b3cf06d58278d9e5dc5c1dbf648e7401
Nicholas George <nicholas.george32@gmail.com>
2018-03-04 10:08:51 -0700

    style changes to main page

--- thermalsnap/main.py
+++ thermalsnap/main.py
@@ -3,24 +3,31 @@
 
 import tkinter as tk
 
+
 class WelcomePage:
     def __init__(self, master):
         self.master = master
         master.title("SnakeSnap v0.1-dev")
-        self.label = tk.Label(master, text="Welcome to SnakeSnap!")
-        self.message = tk.Label(master, text="Login with a gmail account below \nto send email or continue as a guest")
+        master.geometry("600x500")
+        master.configure(background = "white")
+        self.label = tk.Label(master, text="Welcome to SnakeSnap!", bg = "white",
+                              fg = "firebrick3", font = ("arial", 20, "bold"))
+        self.message = tk.Label(master,
+                                text="Login with a gmail account below\n to send email or continue as a guest", bg = "white", font = ("arial", 14, "bold"))
         self.label.grid(columnspan=1)
         self.message.grid(columnspan=3)
         self.greet_button = tk.Button(master, text="Greet", command=self.greet)
         self.greet_button.grid(row=2)
-        self.close_button = tk.Button(master, text="Close", command=master.quit)
+        self.close_button = tk.Button(master, text="Close",
+                                      command=master.quit)
         self.close_button.grid(row=2, column=2)
-
 
     def greet(self):
         print("Greetings")
 
+
 if __name__ == '__main__':
     root = tk.Tk()
+    #root.geometry('600x500')
     snakesnap = WelcomePage(root)
     root.mainloop()
