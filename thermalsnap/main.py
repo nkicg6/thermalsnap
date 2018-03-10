2a87e47bdf645d33644b4fd12dc6ca1d47564510
Nick George <nicholas.george32@gmail.com>
2018-03-10 08:26:36 -0700

    aesthetics and added email and password fields (not functional)

--- thermalsnap/main.py
+++ thermalsnap/main.py
@@ -5,23 +5,29 @@ import tkinter as tk
 
 
 class WelcomePage:
+
     def __init__(self, master):
         self.master = master
         master.title("SnakeSnap v0.1-dev")
         master.geometry("600x500")
         master.configure(background = "white")
-        self.label = tk.Label(master, text="Welcome to SnakeSnap!", bg = "white",
-                              fg = "firebrick3", font = ("arial", 20, "bold"))
-        self.message = tk.Label(master,
-                                text="Login with a gmail account below\n to send email or continue as a guest", bg = "white", font = ("arial", 14, "bold"))
-        self.label.grid(columnspan=1)
-        self.message.grid(columnspan=3)
-        self.greet_button = tk.Button(master, text="Greet", command=self.greet)
-        self.greet_button.grid(row=2)
-        self.close_button = tk.Button(master, text="Close",
-                                      command=master.quit)
-        self.close_button.grid(row=2, column=2)
+        self.master = master
+        self.initialize(master)
 
+    def initialize(self, master):
+        LOGIN_MESSAGE = "Login with a gmail account below\n to send email or continue as a guest"
+        self.label = tk.Label(master, text="Welcome to SnakeSnap!", bg = "white",
+                              fg = "firebrick3", font = ("arial", 30, "bold"), anchor = "center").grid(row=1, column=1)
+        self.message = tk.Label(master,
+                                text= LOGIN_MESSAGE, bg = "white",
+                                font = ("arial", 18, "bold")).grid(row=2, column=1)
+        self.emailLabel = tk.Label(master, text = "Email", bg = "white", fg = "black", font = ("arial", 20, "bold")).grid(row=3)
+        self.emailLabel = tk.Label(master, text = "password", bg = "white", fg = "black", font = ("arial", 20, "bold")).grid(row=4)
+        self.email = tk.Entry(master).grid(row=3, column=1)
+        self.password = tk.Entry(master).grid(row=4, column=1)
+        self.close_button = tk.Button(master, text="Quit",
+                                      command=master.quit).grid(row=5, column=1)
+        self.login_button = tk.Button(master, text="Login").grid(row=5)
     def greet(self):
         print("Greetings")
 
