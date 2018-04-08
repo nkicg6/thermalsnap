4fcbcc04f482b9df9fc396ad8bdbd78fdbf46162
Nick George <nicholas.george32@gmail.com>
2018-04-07 23:02:50 -0600

    OO approach now functional. Ready for snakesnap features

--- thermalsnap/main.py
+++ thermalsnap/main.py
@@ -1,6 +1,9 @@
 #! /usr/bin/python3
 # main tkinter app
 
+# super helpful https://stackoverflow.com/questions/33646605/how-to-access-variables-from-different-classes-in-tkinter
+import os
+import sys
 import tkinter as tk
 import tkinter.font
 from tkinter import ttk, messagebox
@@ -15,6 +18,12 @@ class BaseApp(tk.Tk):
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
+        self.shared_data = {
+            "username" : tk.StringVar(),
+            "password" : tk.StringVar(),
+            "message" : tk.StringVar(),
+        }
+
         self.frames = {}
         self.geometry(winsize)
         self.configure(background="white")
@@ -26,6 +35,7 @@ class BaseApp(tk.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
         self.show_frame(LoginPage)
 
+
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
@@ -35,40 +45,67 @@ class LoginPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
+        self.controller = controller
         # tk labels for stuff with entry points
         mainlabel = tk.Label(self, text="Welcome, please login with a gmail account below",
                          font=TITLE_FONT)
         email = tk.Label(self, text="gmail username", font=LABEL_FONT)
         password = tk.Label(self, text="gmail password", font=LABEL_FONT)
-        email_entry = tk.Entry(self)
-        password_entry = tk.Entry(self, show="*")
+        email_entry = tk.Entry(self, textvariable=self.controller.shared_data["username"])
+        password_entry = tk.Entry(self, show="*", textvariable=self.controller.shared_data["password"])
+
         # tk buttons with callbacks
+
         login_button = ttk.Button(self, text="Login to snakesnap",
-                            command=lambda: controller.show_frame(MainPage))
-
+                            command=self.login_command)
         quit_button = ttk.Button(self, text="Quit", command=self.quit)
+
         # positioning labels and entry points.
+
         mainlabel.grid(row=0, columnspan=3, padx = 10, pady=10)
-        email.grid(row=1, padx=10, pady=10)
-        email_entry.grid(row=1, column=2, columnspan=2, padx=10, pady=10)
-        password.grid(row=2, padx=10, pady=10)
-        password_entry.grid(row=2, column=2, columnspan=2, padx=10, pady=10)
+        email.grid(row=1, columnspan=2,padx=0, pady=10)
+        email_entry.grid(row=1, column=2, columnspan=2, padx=0, pady=10)
+        password.grid(row=2, columnspan=2, padx=10, pady=10)
+        password_entry.grid(row=2, column=2, columnspan=2, padx=0, pady=10)
         login_button.grid(row=3, column=1)
-        quit_button.grid(row=3, column=2)
+        quit_button.grid(row=4, column=1)
 
-
+    def login_command(self):
+        """set the variables, then show the main page
+        """
+        #print(self.controller.shared_data["username"].get())
+        #print(self.controller.shared_data["password"].get())
+        self.controller.show_frame(MainPage)
 
 
 class MainPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="SnakeSnap v0.1-dev", font=TITLE_FONT)
-        label.pack(pady=10,padx=10)
-
+        self.controller = controller
+        main_label = tk.Label(self, text="Welcome!", font=TITLE_FONT)
+        main_label.grid(row=0,columnspan=3, pady=10,padx=10)
         logout_button = ttk.Button(self, text="Logout",
-                            command=lambda: controller.show_frame(LoginPage))
-        logout_button.pack()
+                            command= self.logout)#lambda: controller.show_frame(LoginPage))
+        logout_button.grid(row=1)
+        thermal = ThermalFunctions(parent, controller)
+
+    def logout(self):
+        # not working
+        pass
+        #self.controller.email_entry.delete(0,END)
+        self.controller.show_frame(LoginPage)
+        #LoginPage.password_entry.delete(0,END)
+
+
+class ThermalFunctions:
+    def __init__(self, parent, controller):
+        self.controller = controller
+        if os.uname()[4] != "Linux":
+            print("Pi not detected, debug mode only")
+        else:
+            pass
+
 
 ## constants
 winsize = "600x500"
